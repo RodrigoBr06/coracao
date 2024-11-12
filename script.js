6e24a9d7f05eb1d6f222774884c54a11b718cd0c
RodrigoBr06 <163081265+RodrigoBr06@users.noreply.github.com>
2024-11-11 22:27:17 -0300

    ..

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao=document.querySelector(".btn")
-const nav= document.querySelector(".navv")
+const nav= document.querySelector(".nav")
 
 botao.addEventListener("click", () =>
 nav.classList.toggle("active"))
