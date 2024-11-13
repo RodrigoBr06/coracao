f56c8b94bcf5f2e7ef7b1fc4480a66d52582f539
RodrigoBr06 <163081265+RodrigoBr06@users.noreply.github.com>
2024-11-12 22:33:08 -0300

    .

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ nav.classList.toggle("active"))
 botao.addEventListener("click", function (){
     if(div.style.display==="none"){
         div.style.display="block"
+         
     }else{
         div.style.display="none"
+        
+      
     }
 })
