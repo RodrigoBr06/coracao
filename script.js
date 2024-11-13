5c688f195fbb03283e8b2949361dd75180251049
RodrigoBr06 <163081265+RodrigoBr06@users.noreply.github.com>
2024-11-12 21:57:46 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const botao=document.querySelector(".btn")
 const nav= document.querySelector(".nav")
+const div= document.querySelector("#conteiner")
+
 
 botao.addEventListener("click", () =>
 nav.classList.toggle("active"))
+
+botao.addEventListener("click", function (){
+    if(div.style.display==="none"){
+        div.style.display="block"
+    }else{
+        div.style.display="none"
+    }
+})
